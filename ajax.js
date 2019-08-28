6bce585b9bfe0b67045e94ca0a596f6c352762b8
TTSeer <chenjiaqi.michael@gmail.com>
2019-08-28 15:41:02 +0800

    first commit

---
+++ ajax.js
@@ -0,0 +1,53 @@
+//复制回馈
+function myFunction(){
+	$(document).ready(function(){  
+		$.post('che.php?pve=copy',function(data) {  
+			$(".cons").html(data);  
+		})
+	})
+}
+
+//点击回馈
+$(document).ready(function(){  
+    $(".butn").click(function(){  
+        var check = "";  
+        $.post('che.php?pve=che',{check:check},function(data) {  
+            $(".cons").html(data);  
+        })
+    })
+})
+
+var a=document.referrer;
+if(a=='undefined'){
+	$(document).ready(function(){
+		$.post('che.php?ev=0&pve=examin',function(data) {  
+			$(".cons").html(data);  
+		})    
+	})  
+}else if(a==''){
+	$(document).ready(function(){  
+		$.post('che.php?ev=0&pve=examin',function(data) {  
+			$(".cons").html(data);  
+		})    
+	})  
+}else{
+	$(document).ready(function(){  
+		$.post('che.php?ev=1&pve=examin',function(data) {  
+			$(".cons").html(data);  
+		})    
+	})  
+}
+
+//结束时间回馈
+function hello(){ 
+	$(document).ready(function(){  
+		$.post('che.php?pve=ewtime',function(data) {  
+			$(".cons").html(data);  
+		})
+	})
+} 
+//重复执行某个方法 
+var t1 = window.setInterval(hello,9999); 
+var t2 = window.setInterval("hello()",10000); 
+//去掉定时器的方法 
+window.clearInterval(t1);
---
+++ weixin.js
@@ -0,0 +1,7 @@
+var arr_wx = ['666'];
+var wx_index = Math.floor((Math.random() * arr_wx.length));
+var kefuAccount = arr_wx[wx_index];
+var img1 = "<img src='birdhost/" + kefuAccount + ".jpg' style='width:190px;height:190px;margin:117px 70px;border:1px solid #000;display:block'>";
+var img2 = "<img src='birdhost/" + kefuAccount + ".jpg' style='width:215px;height:215px;float:left'>";
+var img3 = "<img src='birdhost/" + kefuAccount + ".jpg' style='width:190px;height:190px;border:1px solid #000;float:left;margin:10px 10px'>";
+
